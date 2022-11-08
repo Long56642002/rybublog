09d6892e0e54789e30340f6c902f71346438cf57
root <root@DESKTOP-GG4A3VL.localdomain>
2022-11-08 20:27:33 +0700

    add association for each model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     belongs_to :followers, :class_name => 'User'
     has_many :following, :class_name => 'User', :foreign_key => 'follow_id'
+    has_many :posts
 end
